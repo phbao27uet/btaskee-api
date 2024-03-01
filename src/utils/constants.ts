ebd45bb0e5f63320cee6a7d595d1d75189007300
baopham <baopq@solashi.com>
2024-03-01 13:43:44 +0900

    log: update send notification

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -24,6 +24,61 @@ export const WEBHOOKS_DISCORD = {
   DISCORD_6: process.env.ZEN_DISCORD_6,
 };
 
+export const WEBHOOKS_DISCORD_1 = {
+  DISCORD_0: process.env.ZEN_DISCORD_0,
+  DISCORD_0_1: process.env.ZEN_DISCORD_0_1,
+  DISCORD_1_2: process.env.ZEN_DISCORD_1_2,
+  DISCORD_2_3:
+    'https://discord.com/api/webhooks/1212952287488512030/2Y9u2KiBU4BLutanqleV_ZJL10PH_RYHXoRQ62Gybz1oM9cPBTLv09FK2ZtlEtQGmHrt',
+  DISCORD_3_4:
+    'https://discord.com/api/webhooks/1212952309156552735/Z4SPkpXb23b6aMkuuOmpMEHOzwnXC-sKK2MB_6R0aWCX13_UPFXCm3U5UDddt9Ir1yu0',
+  DISCORD_4_5:
+    'https://discord.com/api/webhooks/1212952326923620425/uhrA0gM9OoerVcrRtPccjSQx-fHyx44GurtvDHc8QbbpHZMFP3ATm2mBC7gtCpE3O1pW',
+  DISCORD_5_6:
+    'https://discord.com/api/webhooks/1212952348645785620/u3lW88KmytTlhcn1if9vdHK_bvF-LVF_DaXyGkpk7qcBjWvSvlWy8LU_fkuxhNYWUAQz',
+  DISCORD_6:
+    'https://discord.com/api/webhooks/1212952369940136047/_AfYYUfDSkcQqOHvsTT6yCQqDtIPpLvV0jEYp2m1h-k3upzGYCXSWYtauKVV2Qo6_CQa',
+};
+
+export const WEBHOOKS_DISCORD_2 = {
+  DISCORD_0: process.env.ZEN_DISCORD_0,
+  DISCORD_0_1: process.env.ZEN_DISCORD_0_1,
+  DISCORD_1_2: process.env.ZEN_DISCORD_1_2,
+  DISCORD_2_3:
+    'https://discord.com/api/webhooks/1212952168630583327/n5oMFPsdQYbr6HYmZfY0ZOwfIXmmdDcQH2LfmaX8ltpMfRDmseP9CB3_6nQnsPyaq5Oo',
+  DISCORD_3_4:
+    'https://discord.com/api/webhooks/1212952195738243122/GSGqfx4RxASNdV2PQ-fZN_67faBQ7RC1Lgcbp5IvxjW-BqbM6jPwrwBR8327RMEefo7N',
+  DISCORD_4_5:
+    'https://discord.com/api/webhooks/1212952217305350165/RVVymZpRyKjxGc9migzltqvOdYe4stfGh4ZsImlGQRJ_doYHVl7TpcdzHQ5J5oLsgd6v',
+  DISCORD_5_6:
+    'https://discord.com/api/webhooks/1212952241028333628/OnEBkOOiW8kd9NiI5kaAd-O6Z6-UW53C5io4BRC-MRwMtsJImTPSyqz2Sw1Gw-Uhh93i',
+  DISCORD_6:
+    'https://discord.com/api/webhooks/1212952263115538504/aUoPRhJnHUfyjERJNtLRSw_cnfsHC95kHog5uDimmOJ0s_V3E_F9b626iwytL6E85DDh',
+};
+
+export const WEBHOOKS_DISCORD_3 = {
+  DISCORD_0: process.env.ZEN_DISCORD_0,
+  DISCORD_0_1: process.env.ZEN_DISCORD_0_1,
+  DISCORD_1_2: process.env.ZEN_DISCORD_1_2,
+  DISCORD_2_3:
+    'https://discord.com/api/webhooks/1212952009951416391/RyvrwA67KckQOfUP3fyDMjJfEJ1kGjmTuMOh8uAk5meuxgtTKg9g_2hYbBF7jERzU2hL',
+  DISCORD_3_4:
+    'https://discord.com/api/webhooks/1212952056172908564/uPX0NZBC8HzWR5QesmE92DAKl3qaUcgstfZej26fRARLIN00bZ2YT_daycVFUDtLbWeo',
+  DISCORD_4_5:
+    'https://discord.com/api/webhooks/1212952080860446760/xUBmqAI6bIJOBkTTvURfyXqv7k7vGZ3fERh_okBAeiom8oW-eFwFRsZyFxiIEH-k5D_s',
+  DISCORD_5_6:
+    'https://discord.com/api/webhooks/1212952106923728966/JVa5hI2a17nQYd7gwo23DzTlQXMId6K62wFTybTbshYGxkp4Nr84uSWFP4Hmu_5j5xUv',
+  DISCORD_6:
+    'https://discord.com/api/webhooks/1212952137416450080/u2VCHDI15XcHbw6PlCP4r-92fMpT13tdcOkKf_GVPCwxR1xPdDotM240PU4_Hr70-Uuu',
+};
+
+export const WEBHOOKS_DISCORDS = [
+  WEBHOOKS_DISCORD,
+  WEBHOOKS_DISCORD_1,
+  WEBHOOKS_DISCORD_2,
+  WEBHOOKS_DISCORD_3,
+];
+
 export const API_URL = process.env.ZEN_API_URL;
 
 export const RUNNING_COUNT = {
